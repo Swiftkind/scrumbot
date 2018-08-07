a7a53df484c05c5e6a53dd4932a4aa57ccc2ff56
Chibi-Shem <carl.ipanag@gmail.com>
2018-08-07 17:49:18 +0800

    1.2.1.1

--- assets/fe/dist/out-tsc/src/app/utils/layouts.utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function ContentOnly(content) {
-    return {
-        content: content
-    };
-}
-exports.ContentOnly = ContentOnly;
-//# sourceMappingURL=layouts.utils.js.map
