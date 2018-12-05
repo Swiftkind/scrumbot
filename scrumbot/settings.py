2fd82a9ec5c0bfb78b2cd115fb6a8d32c73f7e28
cholorecto <pocholorecto123@gmail.com>
2018-12-05 17:29:16 +0800

    Redirect to login the user if the token is expire

--- scrumbot/settings.py
+++ scrumbot/settings.py
@@ -124,7 +124,7 @@ REST_FRAMEWORK = {
 }
 
 JWT_AUTH = {
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=72),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=30),
     'JWT_ALLOW_REFRESH': True,
 }
 
